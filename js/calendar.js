1638a824803db42ee2ee589527fc2a499cf05606
scrmbld <liamallen792@gmail.com>
2020-07-20 20:57:19 -0700

    created basic html structure

--- js/calendar.js
+++ js/calendar.js
@@ -1,1 +1,25 @@
-alert("Hello World!");
+//day class
+class Day {
+  constructor(id, parent) {//int, parent Calendar id
+    this.id = id;
+    this.element = document.createElement(`div`);
+    this.element.id = `n${id}`;
+    this.element.classList.add(`day`);
+    document.querySelector(`.calendar-container#${parent} .week#n${Math.floor(id / 7)}`).appendChild(this.element);
+    this.element = document.querySelector(`.calendar-container #${parent} .week #n${id}`);
+  }
+}
+
+class Calendar {
+  constructor(id) {
+    this.id = id;
+    this.element = document.querySelector(`.calendar-container #${id}`);
+    this.dayList = [];
+    for (let i = 0; i < 35; i++) {
+      this.dayList.push(new Day(i, id));
+    }
+  }
+}
+
+//generate the calendar
+var calendar = new Calendar(`n1`);
